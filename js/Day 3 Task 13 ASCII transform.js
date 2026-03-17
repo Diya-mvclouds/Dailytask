2f9cf223f608efe56857e6f4425be01a7317cacc
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-17 18:18:31 +0530

    js

--- js/Day 3 Task 13 ASCII transform.js
+++ js/Day 3 Task 13 ASCII transform.js
@@ -3,6 +3,12 @@ let str = prompt("Enter a sentence: ");
 let a = "";
 for (const char of str){
     let ch = char.charCodeAt(0);
-    a += ch % 2 == 0 ? char.toUpperCase() : char.toLowerCase();
+    // a += ch % 2 == 0 ? char.toUpperCase() : char.toLowerCase();
+    if(ch % 2 == 0){
+        a += char.toUpperCase()
+    }
+    else{
+        a+= char.toLowerCase()
+    }
 }
 console.log(a);
