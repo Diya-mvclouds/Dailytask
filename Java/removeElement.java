f1fdd142ba3609c854d3d6d04c497fe2e458727b
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-18 12:24:10 +0530

    Remove Element

--- Java/removeElement.java
+++ Java/removeElement.java
@@ -1,30 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class removeElement {
-    static void remove(int arr[], int n, Scanner sc){
-        int r = 0;
-        System.out.println("Enter an elements in array: ");
-        for (int i = 0; i <= 2*n; i++) {
-            if(i < n){
-                arr[i] = sc.nextInt();
-            }
-            else if(i == n){
-                System.out.println("Enter a number to remove: ");
-                r = sc.nextInt();
-                System.out.println("After remove: ");
-            }
-            else{
-                if(arr[i-(n+1)] != r){
-                    System.out.print(arr[i-(n+1)] + " ");
-                }
-            }
-        }
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a size of array: ");
         int n = sc.nextInt();
-        int arr[] = new int[n];
-        remove(arr, n, sc);
+        ArrayList<Integer> arr = new ArrayList<>();
+        System.out.println("Enter an elements in array: ");
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        System.out.println("Enter a number to remove: ");
+        int r = sc.nextInt();
+        arr.remove(Integer.valueOf(r));
+        System.out.println(arr);
     }
 }
