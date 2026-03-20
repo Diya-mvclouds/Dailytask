a4d20f2c0c0fed642b7452f69f62b67a98832adc
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-20 17:35:18 +0530

    Remaining time

--- js_to_java/remainingDays.java
+++ js_to_java/remainingDays.java
@@ -1,14 +1,23 @@
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class remainingDays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a future date(YYYY-MM-DD): ");
+        LocalDate now = LocalDate.now();
         LocalDate future = LocalDate.parse(sc.nextLine());
-        LocalDate today = LocalDate.now();
-        long days = ChronoUnit.DAYS.between(today, future);
-        System.out.println("Remaining days are " + days);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+        System.out.println("Current Date: " + now.format(fmt));
+        System.out.println("Future Date: " + future.format(fmt));
+        if (now.isAfter(future)) {
+            System.out.println("Time is already passed...!!!");
+        }
+        else{
+            Period diff = Period.between(now, future);
+            System.out.println("Remaining time: ");
+            System.out.println(diff.getYears() + " Years " + diff.getMonths() + " Months " + diff.getDays() + " Days");
+        }
     }
 }
