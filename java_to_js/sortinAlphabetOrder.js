2b2e525ba9f905b8d994710d0ca081a5ea91fd29
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-26 15:26:33 +0530

    sort in alphabetic order

---
+++ java_to_js/sortinAlphabetOrder.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+let str = prompt("Enter a word: ");
+let arr = str.split("");
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let a = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = a;
+        }   
+    }    
+}
+console.log("After sorting:", arr.join(""))
