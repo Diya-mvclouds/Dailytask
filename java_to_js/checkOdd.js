0cf564eae7e13015c85daa083a194415f5a24c76
Diya-mvclouds <diyashahgu@gmail.com>
2026-03-26 15:15:57 +0530

    Butterfly pattern

---
+++ java_to_js/checkOdd.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")();
+let num = prompt("Enter a numbers: ");
+let n = num.split(" ");
+console.log(n);
+let odd = true;
+
+for(let i of n){
+    if(i % 2 == 0){
+        odd = false;
+    }
+}
+
+if(odd){
+    console.log("It contains only odd numbers");
+}
+else{
+    console.log("It contains even numbers")
+}
