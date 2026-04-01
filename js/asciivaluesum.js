78944f3a1a5be2b2a521e4acba02ec88ef11b388
Diya-mvclouds <diyashahgu@gmail.com>
2026-04-01 14:53:30 +0530

    ascii value sum

--- js/asciivaluesum.js
+++ js/asciivaluesum.js
@@ -1,11 +1,10 @@
-const prompt = require('prompt-sync')();
-let str = prompt("Enter a string:");
-let arr = str.split("");
-console.log(arr);
-let b = 0;
-for(const char of arr){
-    const ch = char.charCodeAt(0);
-    let a = Number(String(ch));
-    b += a;
+function asciiSum(str){
+    let arr = str.split("");
+    let b = 0;
+    for(const char of arr){
+        const ch = char.charCodeAt(0);
+        let a = Number(String(ch));
+        b += a;
+    }
+    return "Sum of ASCII value: " + b;
 }
-console.log(b)
