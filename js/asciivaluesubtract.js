67d757e52ae82f089467e391fefcde4aed28b637
Diya-mvclouds <diyashahgu@gmail.com>
2026-04-01 14:53:19 +0530

    ascii value subtraction

--- js/asciivaluesubtract.js
+++ js/asciivaluesubtract.js
@@ -1,19 +1,21 @@
-const prompt = require('prompt-sync')();
-let str = prompt("Enter a string:");
-let arr = str.split("");
-console.log(arr);
-let b = 0;
-let c = 128;
-for(const char of arr){
-    const ch = char.charCodeAt(0);
-    let a = Number(ch)
+function asciiSubtract(str){
+    let arr = str.split("");
+    let b = 0;
+    let c = 128;
+    let output = "";
+    for(const char of arr){
+        const ch = char.charCodeAt(0);
+        let a = Number(ch);
         b -= a;
-        console.log(char ,a)
-    if (a < c) {
-        c = a;
-    }    
+        output += char + " " + a + "<br>";
+
+        if (a < c) {
+            c = a;
+        }    
+    }
+    if(b <= 0){
+        output += "Smallest ASCII value: " + c + "<br>";
+    }
+    output += "Subtraction of ASCII values: " + b;
+    return output;
 }
-if(b <= 0){
-    console.log("Smallest ASCII value:", c);
-}
-console.log("Subtraction of ASCII values:", b);
